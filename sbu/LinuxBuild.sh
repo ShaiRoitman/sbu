d779df469e5450534d0ad97e600806641b2fcf80
Shai Roitman <Shai@Roitman.info>
2019-05-25 20:10:56 +0000

    Linux

--- sbu/LinuxBuild.sh
+++ sbu/LinuxBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./ResourceBuild.sh
-cmake -DCMAKE_PREFIX_PATH=/home/master/sbu/sbu/cmake/linux -DSBU_PLATFORM=windows . 
+cmake -DCMAKE_PREFIX_PATH=/home/master/sbu/sbu/cmake/linux -DSBU_PLATFROM=linux . 
 make -j 6
