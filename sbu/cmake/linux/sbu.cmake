db909751934221e0934d072e9dcbd8c7e88304fa
Shai Roitman <Shai@Roitman.info>
2019-05-25 19:36:28 +0000

    Linux Compile

---
+++ sbu/cmake/linux/sbu.cmake
@@ -0,0 +1,5 @@
+#set (POCO_DEP Poco::Foundation Poco::Util Poco::Crypto Poco::Zip)
+#set (AWS_DEP aws-cpp-sdk-s3 aws-cpp-sdk-core aws-cpp-sdk-transfer)
+
+set (POCO_DEP libPocoZip libPocoFoundation libPocoUtil libPocoCrypto)
+set (AWS_DEP aws-cpp-sdk-s3 aws-cpp-sdk-core aws-cpp-sdk-transfer)
