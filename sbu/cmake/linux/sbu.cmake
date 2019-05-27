3108d93b3c71e8b03a73a0edfba2a743d975eb1d
Shai Roitman <Shai@Roitman.info>
2019-05-27 08:57:00 +0300

    not yet ready

--- sbu/cmake/linux/sbu.cmake
+++ sbu/cmake/linux/sbu.cmake
@@ -1,5 +1,6 @@
 set (POCO_DEP PocoZip PocoFoundation PocoUtil PocoCrypto)
 set (AWS_DEP aws-cpp-sdk-s3 aws-cpp-sdk-core aws-cpp-sdk-transfer)
+set (SBU_PLATFORM_LIBS boost_filesystem boost_program_options aws-checksums dl pthread)
 
 link_directories("/home/master/3rdParty/boost_1_70_0/stage/lib")
 link_directories("/home/master/3rdParty/poco/rel/lib")
--- sbu/cmake/windows/sbu.cmake
+++ sbu/cmake/windows/sbu.cmake
@@ -1,2 +1,3 @@
 set (POCO_DEP Poco::Zip Poco::Foundation Poco::Crypto Poco::Util)
 set (AWS_DEP aws-cpp-sdk-s3 aws-cpp-sdk-core aws-cpp-sdk-transfer)
+set (SBU_PLATFORM_LIBS)
