5d4bd8ead241641edf6400893c4d866f9e83f2a7
Shai Roitman <Shai@Roitman.info>
2019-05-28 15:56:12 +0300

    Linux Compile

--- sbu/cmake/linux/PocoConfig.cmake
+++ sbu/cmake/linux/PocoConfig.cmake
@@ -1,3 +1,5 @@
 include_directories(/home/master/3rdParty/poco/Foundation/include)
 include_directories(/home/master/3rdParty/poco/Zip/include)
 include_directories(/home/master/3rdParty/poco/Crypto/include)
+include_directories(/home/master/3rdParty/poco/Net/include)
+include_directories(/home/master/3rdParty/poco/Util/include)
