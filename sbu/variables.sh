e0a0a480a6aea7059dd2520c094632e40093b0c1
Shai Roitman <Shai@Roitman.info>
2021-10-04 17:17:29 +0300

    remove sqlite3 explict link and boost dir dependency

--- sbu/variables.sh
+++ sbu/variables.sh
@@ -2,6 +2,5 @@
 export GIT_ROOT=/git
 export SBU_ROOT=/sbu
 
-export BOOST_ROOT=$GIT_ROOT/boost_1_72_0
 export CompileTOCPP=$GIT_ROOT/CompileToCppVar/CompileToCppVariable
 export ROOT_PATH=$SBU_ROOT/sbu/SbuCli/resources/
