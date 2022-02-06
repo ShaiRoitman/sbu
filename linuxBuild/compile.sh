85887a8757b1adacdf7ddaaad30fa5feec0ea69e
Shai Roitman <Shai@Roitman.info>
2022-02-06 14:11:57 +0200

    sbu_run

--- linuxBuild/compile.sh
+++ linuxBuild/compile.sh
@@ -4,10 +4,16 @@ cp /sbu/linuxBuild/SQLiteCppConfig.cmake /usr/local/lib/cmake/SQLiteCpp/SQLiteCp
 cd /sbu/sbu
 ./LinuxBuild.sh
 mkdir sbu_app
-ldd SbuCli/sbu | grep "=> /" | awk '{print $3}' | xargs -I '{}' cp -v '{}' sbu_app/
+mkdir sbu_app/so
+ldd SbuCli/sbu | grep "=> /" | awk '{print $3}' | xargs -I '{}' cp -v '{}' sbu_app/so
+mv sbu_app/so/sbu sbu_app/sbu
 cp SbuCli/sbu sbu_app
-cd sbu_app
+pushd sbu_app/so
 strip *
+popd
+pushd sbu_app
+strip sbu
+popd
 
 
 
